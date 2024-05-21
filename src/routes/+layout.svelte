b0ab949c3fc1867b50c58c96385fd84a8d542138
BobbyNooby <theengineering.commandgamer@gmail.com>
2024-05-21 11:16:54 +0100

    Meta tags

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,6 +3,35 @@
 	import '../app.css';
 </script>
 
+<head>
+	<!-- Primary Meta Tags -->
+	<title>{'>bobbynooby.dev'}</title>
+	<meta name="title" content="bobbynooby.dev" />
+	<meta name="description" content="" />
+
+	<!-- Open Graph / Facebook -->
+	<meta property="og:type" content="website" />
+	<meta property="og:url" content="https://bobbynooby.dev/" />
+	<meta property="og:title" content=">bobbynooby.dev" />
+	<meta property="og:description" content="" />
+	<meta
+		property="og:image"
+		content="https://raw.githubusercontent.com/BobbyNooby/bobbynooby.dev/master/static/logonotransparent.jpg"
+	/>
+
+	<!-- Twitter -->
+	<meta property="twitter:card" content="summary_large_image" />
+	<meta property="twitter:url" content="https://bobbynooby.dev/" />
+	<meta property="twitter:title" content=">bobbynooby.dev" />
+	<meta property="twitter:description" content="" />
+	<meta
+		property="twitter:image"
+		content="https://raw.githubusercontent.com/BobbyNooby/bobbynooby.dev/master/static/logonotransparent.jpg"
+	/>
+
+	<!-- Meta Tags Generated with https://metatags.io -->
+</head>
+
 <section class="overflow-auto app items-center">
 	<main><slot /><AfkChecker /></main>
 </section>
